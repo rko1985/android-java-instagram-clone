2900c329538bd0e2222ebdb616772afbddebb626
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-03-03 06:08:25 -1000

    query select all

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
@@ -25,6 +26,8 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,36 +37,19 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-//    ParseObject score = new ParseObject("Score");
-//    score.put("username", "sean");
-//    score.put("score", 65);
-//    score.saveInBackground(new SaveCallback() {
-//      @Override
-//      public void done(ParseException e) {
-//        if(e == null){
-//          //Ok
-//          Log.i("Success", "We saved the score");
-//        } else {
-//          e.printStackTrace();
-//        }
-//      }
-//    });
-
-
-
     ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
-    query.getInBackground("uICn4H1KYf", new GetCallback<ParseObject>() {
-      @Override
-      public void done(ParseObject object, ParseException e) {
-        if(e == null && object != null){
-
-          object.put("score", 85);
-          object.saveInBackground();
-
-          Log.i("username", object.getString("username"));
-          Log.i("score", Integer.toString(object.getInt("score")));
+    query.findInBackground(new FindCallback<ParseObject>() {
+        @Override
+        public void done(List<ParseObject> objects, ParseException e) {
+            if(e == null){
+                if(objects.size() > 0){
+                    for(ParseObject object : objects){
+                        Log.i("username", object.getString("username"));
+                        Log.i("score", Integer.toString(object.getInt("score")));
+                    }
+                }
+            }
         }
-      }
     });
 
 
